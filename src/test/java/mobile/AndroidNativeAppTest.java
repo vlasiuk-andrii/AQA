893550d65290bc0615c9fd0ea50c751272b0a102
vlasiuk-andrii <vlasiuk.andrii93@gmail.com>
2019-02-27 14:48:58 +0200

    Factory of MobileElements created

--- src/test/java/mobile/AndroidNativeAppTest.java
+++ src/test/java/mobile/AndroidNativeAppTest.java
@@ -1,11 +1,13 @@
 package mobile;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.Assert;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -14,7 +16,20 @@ import properties.PropertiesHolder;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static org.testng.Assert.assertEquals;
+
 public class AndroidNativeAppTest {
+
+    @AndroidFindBy(xpath = "//android.widget.Button[contains(@resource-id, 'digit_2')]")
+    public MobileElement digit2;
+    @AndroidFindBy(xpath = "//android.widget.Button[contains(@resource-id, 'digit_3')]")
+    public MobileElement digit3;
+    @AndroidFindBy(xpath = "//android.widget.Button[contains(@resource-id, 'op_add')]")
+    public MobileElement plusButton;
+    @AndroidFindBy(xpath = "//android.widget.Button[contains(@resource-id, 'eq')]")
+    public MobileElement equalButton;
+    @AndroidFindBy(xpath = "//android.widget.TextView[contains(@resource-id, 'result')]")
+    public MobileElement resultField;
 
     AppiumDriver driver;
 
@@ -25,23 +40,23 @@ public class AndroidNativeAppTest {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_API_28");
         capabilities.setCapability(MobileCapabilityType.UDID, "emulator-5554");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0");
         capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "100");
         capabilities.setCapability("appPackage", "com.android.calculator2");
         capabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
 
         driver = new AndroidDriver(serverAddress, capabilities);
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
     @Test
-    public void smokeTest() {
-        driver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_2')]")).click();
-        driver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'op_add')]")).click();
-        driver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'digit_3')]")).click();
-        driver.findElement(By.xpath("//android.widget.Button[contains(@resource-id, 'eq')]")).click();
+    public void simpleSummTest() {
+        digit2.click();
+        plusButton.click();
+        digit3.click();
+        equalButton.click();
 
-        Assert.assertEquals("5", driver.findElement(By.xpath("//android.widget.TextView[contains(@resource-id, 'result')]")).getText());
+        assertEquals("5", resultField.getText());
     }
 
     @AfterTest
