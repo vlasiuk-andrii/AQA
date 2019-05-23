f9b17344f35a77326e0ff45cb04ae2646629c61a
vlasiuk-andrii <vlasiuk.andrii93@gmail.com>
2019-05-23 15:59:54 +0300

    Api tests fixed - json examples moved to core module

--- core/src/main/java/main/core/utils/JsonUtils.java
+++ core/src/main/java/main/core/utils/JsonUtils.java
@@ -3,6 +3,7 @@ package main.core.utils;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.nio.file.Path;
@@ -15,7 +16,8 @@ public class JsonUtils {
     public static JsonElement getJsonFromFile(Path path) {
         JsonElement jsonElement = null;
         try {
-            reader = new FileReader(System.getProperty("user.dir") + "\\target\\test-classes" + path);
+            File file = new File(System.getProperty("user.dir")).getParentFile();
+            reader = new FileReader(file.getAbsolutePath() + "\\core\\target\\classes" + path);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
