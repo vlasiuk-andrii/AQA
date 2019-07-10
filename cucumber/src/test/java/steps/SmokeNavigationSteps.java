f02a683d574256bcd1263f0ff85566929da9bb3e
vlasiuk-andrii <vlasiuk.andrii93@gmail.com>
2019-07-10 16:35:49 +0300

    SmokeNavigation test added for cucumber testsuite

---
+++ cucumber/src/test/java/steps/SmokeNavigationSteps.java
@@ -0,0 +1,51 @@
+package steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import test.ui.flow.DragAndDropFlow;
+import test.ui.pages.DragAndDropPage;
+
+import static com.codeborne.selenide.Selenide.page;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class DragAndDropSteps {
+
+    DragAndDropFlow dragAndDropFlow = new DragAndDropFlow();
+
+    @Given("^user is on dragAndDrop page$")
+    public void user_is_on_dragAndDrop_page() {
+        dragAndDropFlow.navigate(page(DragAndDropPage.class).getUrl());
+    }
+
+    @Given("^success message is not shown$")
+    public void success_message_is_not_shown() {
+        assertFalse(dragAndDropFlow.successMessageIsShown());
+    }
+
+    @When("^user drop debit account \"([^\"]*)\"$")
+    public void user_drop_debit_account(String debitAccount) {
+        dragAndDropFlow.putDebitAccount(debitAccount);
+    }
+
+    @When("^user drop debit amount \"([^\"]*)\"$")
+    public void user_drop_debit_amount(String debitAmount) {
+        dragAndDropFlow.putDebitAmount(debitAmount);
+    }
+
+    @When("^user drop credit account \"([^\"]*)\"$")
+    public void user_drop_credit_account(String creditAccount) {
+        dragAndDropFlow.putCreditAccount(creditAccount);
+    }
+
+    @When("^user drop credit amount \"([^\"]*)\"$")
+    public void user_drop_credit_amount(String creditAmount) {
+        dragAndDropFlow.putCreditAmount(creditAmount);
+    }
+
+    @Then("^success message is shown$")
+    public void success_message_is_shown() {
+        assertTrue(dragAndDropFlow.successMessageIsShown());
+    }
+}
---
+++ cucumber/src/test/resources/smokeNavigation.feature
@@ -0,0 +1,18 @@
+Feature: Navigation to few pages is working
+
+  Scenario: User can navigate to homePage
+    When user navigate to homePage
+    Then title of homePage is correct
+    And login form is present
+
+  Scenario: User can navigate to newToursPage
+    Given user is on homePage
+    When user click on newToursButton
+    Then title of newToursPage is correct
+    And login form is present
+
+  Scenario: User can navigate to tablePage
+    Given user is on homePage
+    When user click on tableDemoLink
+    Then title of tablePage is correct
+    And table is present
